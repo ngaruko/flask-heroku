c2954ec3fce5c59b181cf97d0f23cfd366fcd78b
ngaruko <bede.ngaruko@gmail.com>
2020-08-28 20:36:25 +1200

    addeded dependencies

--- worldbankapp/routes.py
+++ worldbankapp/routes.py
@@ -2,20 +2,58 @@ from worldbankapp import app
 import json, plotly
 from flask import render_template
 from wrangling_scripts.wrangle_data import return_figures
+##
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar 
+import joblib
+from sqlalchemy import create_engine
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    # figures = return_figures()
+    # genre_counts = df.groupby('genre').count()['message']
+    # genre_names = list(genre_counts.index)
+    # print(genre_names)
+    # category_names = df.columns[4:]
+    # print(category_names)
+    # category_counts =df.groupby(['request','offer','money','storm']).count()['message']
 
-    # # plot ids for the html id tag
-    # ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+    # # create visuals
+    # # TODO: Below is an example - modify to create your own visuals
+    # graphs = [
+    #     {
+    #         'data': [
+    #             Bar(
+    #                 x=category_names,
+    #                 y=category_counts
+    #             )
+    #         ],
 
-    # # Convert the plotly figures to JSON for javascript in html template
-    # figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # return render_template('index.html',
-    #                        ids=ids,
-    #                        figuresJSON=figuresJSON)
+    #         'layout': {
+    #             'title': 'Distribution of Message Categories',
+    #             'yaxis': {
+    #                 'title': "Count"
+    #             },
+    #             'xaxis': {
+    #                 'title': "category"
+    #             }
+    #         }
+    #     }
+    # ]
+    
+    # # encode plotly graphs in JSON
+    # ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    # graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # # render web page with plotly graphs
+    # return render_template('master.html', ids=ids, graphJSON=graphJSON)                     figuresJSON=figuresJSON)
     return "Hello Heroku"
